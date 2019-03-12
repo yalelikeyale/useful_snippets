37825222e703bcbc6d723079e61f633225feb5e2
Yale Newman <DataGata@Yales-MacBook-Pro.local>
2019-03-11 22:37:30 -0600

    query parameters key lookup function

---
+++ qryParams_lookup.js
@@ -0,0 +1,8 @@
+(function(){
+	var qryParams = {};
+	window.document.location.search.substr(1).split('&').forEach(function(kv){
+	  [key, value] = kv.split('=');
+	  qryParams[key] = value;
+	});
+	return qryParams.category;
+})()
